414fe3869e3cf9b925f238e0185a51be26a0f994
nipun deelaka <46063855+nipdep@users.noreply.github.com>
2021-09-05 19:32:00 +0530

    init commit

---
+++ run.py
@@ -0,0 +1,16 @@
+from defom.factory import create_api
+
+import os
+import configparser
+
+config = configparser.ConfigParser()
+config.read(os.path.abspath(os.path.join(".ini")))
+
+if __name__ == '__main__':
+    app = create_api()
+    app.config['DEBUG'] = True
+    app.config['DB_URI'] = config['PROD']['DB_URI']
+    app.config['NS'] = config['PROD']['NS']
+    app.config['SECRET_KEY'] = config['PROD']['SECRET_KEY']
+
+    app.run()
