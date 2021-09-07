16a973b9a0e2fb40705f9d62fc0b5bfa797a1193
nipun deelaka <46063855+nipdep@users.noreply.github.com>
2021-09-07 17:36:21 +0530

    add : [Functionalty]  get daily satellite feed

--- defom/api/forests.py
+++ defom/api/forests.py
@@ -3,7 +3,9 @@
 from flask import make_response, request, jsonify
 from flask_restful import Resource, fields, marshal_with
 
+from defom.api.utils import expect
 from defom.db import save_forest, save_forestTile
+from defom.src.SentinelhubClient import SentilhubClient
 
 class Forest(object):
 
@@ -26,14 +28,31 @@ class ForestTile(object):
         try:
             save_forestTile(forest_id, tile_coordinates)
         except Exception as e:
-            return jsonify({'error': str(e)}), 400
+            return make_response(jsonify({'error': str(e)}), 400)
 
 class RegisterForest(Resource):
-    ## TODO
-    def post(self):
+    def post(self):        
         try:
             post_data = request.get_json()
-
+            forest_data = {}
+            forest_data['forest_name'] = expect(post_data['name'], str, 'forest')
+            forest_data['district'] = expect(post_data['district'], str, 'district')
+            forest_data['country'] = expect(post_data['country'], str, 'country')
+            forest_data['boundary'] = expect(post_data['forest_boundary'], dict, 'forest_boundary')
         except Exception as e:
-            return jsonify({'error': str(e)}), 400
+            return make_response(jsonify({'error': str(e)}), 400)
         
+        try:
+            boundary_coords = forest_data['boundary']
+            sentinel_client = SentilhubClient()
+            bbox_doc_list = sentinel_client.split_forest_area(boundary_coords)
+            forest_data['forest_tiles'] = bbox_doc_list
+        except Exception as e:
+            return make_response(jsonify({'error': str(e)}), 400)
+
+
+        try:
+            res = save_forest(forest_data)
+            return make_response(jsonify({"status" : str(res.acknowledged)}), 200)
+        except Exception as e:
+            return make_response(jsonify({'error': str(e)}), 411)
