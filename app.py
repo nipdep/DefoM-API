df941939d05c13f64a4aa7815748eccbf3d3a50a
nipun deelaka <46063855+nipdep@users.noreply.github.com>
2021-09-11 15:38:30 +0530

    update : app.py

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ import configparser
 config = configparser.ConfigParser()
 config.read(os.path.abspath(os.path.join(".ini")))
 
-if __name__ == '__main__':
-    app = create_api()
-    # app.config['DEBUG'] = True
-    app.config['DB_URI'] = config['PROD']['DB_URI']
-    app.config['NS'] = config['PROD']['NS']
-    app.config['SECRET_KEY'] = config['PROD']['SECRET_KEY']
+# if __name__ == '__main__':
+app = create_api()
+# app.config['DEBUG'] = True
+app.config['DB_URI'] = config['PROD']['DB_URI']
+app.config['NS'] = config['PROD']['NS']
+app.config['SECRET_KEY'] = config['PROD']['SECRET_KEY']
 
-    app.run()
+app.run()
