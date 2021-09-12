85381231c1f15cd15981a451876e71ddd2b9ba69
nipun deelaka <46063855+nipdep@users.noreply.github.com>
2021-09-12 19:20:02 +0530

    update :APP

--- app.py
+++ app.py
@@ -46,34 +46,34 @@ class Fac(Resource):
     def get(self):
         return {"data" : "Hello, World"}
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
     # APP_DIR = os.path.abspath(os.path.dirname(__file__))
 
-    app = Flask(__name__)
-    api = Api(app)
-    app.json_encoder = MongoJsonEncoder
-    jwt = JWTManager(app)
+app = Flask(__name__)
+api = Api(app)
+app.json_encoder = MongoJsonEncoder
+jwt = JWTManager(app)
 
-    # @jwt.user_claims_loader
-    # def add_claims(identity):
-    #     return {
-    #         'user': identity,
-    #     }
+# @jwt.user_claims_loader
+# def add_claims(identity):
+#     return {
+#         'user': identity,
+#     }
 
 
-    app.config['JWT'] = jwt
-    app.config['BCRYPT'] = Bcrypt(app)
-    # app.config['CLAIMS_LOADER'] = add_claims
-    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=30)
+app.config['JWT'] = jwt
+app.config['BCRYPT'] = Bcrypt(app)
+# app.config['CLAIMS_LOADER'] = add_claims
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=30)
 
-    api.add_resource(Fac, '/enter')
-    api.add_resource(User, '/user/<string:name>')
-    api.add_resource(RegisterForest, '/forest/register')
-    api.add_resource(RegisterUser, '/user/register')
-    api.add_resource(LoginUser, '/user/login')
-    api.add_resource(logoutUser, '/user/logout')
-    api.add_resource(GetTiles, '/gettiles')  ## testing resources
-    api.add_resource(MakeClassInf, '/classinf') ## testing resources
+api.add_resource(Fac, '/enter')
+api.add_resource(User, '/user/<string:name>')
+api.add_resource(RegisterForest, '/forest/register')
+api.add_resource(RegisterUser, '/user/register')
+api.add_resource(LoginUser, '/user/login')
+api.add_resource(logoutUser, '/user/logout')
+api.add_resource(GetTiles, '/gettiles')  ## testing resources
+api.add_resource(MakeClassInf, '/classinf') ## testing resources
 
-    app.run()
+app.run()
