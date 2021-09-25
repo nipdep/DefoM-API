839e99e7daa7fe45f59790737fbaed1d4ec918f7
nipun deelaka <46063855+nipdep@users.noreply.github.com>
2021-09-25 23:43:51 +0530

    complete: Apscheduler invoker

--- clock1.py
+++ clock1.py
@@ -21,7 +21,7 @@ logging.basicConfig()
 logger = logging.getLogger('apscheduler')
 logger.setLevel(logging.DEBUG)
 
-CLASS_INPUT_SHAPE = (224, 224)
+CLASS_INPUT_SHAPE = (241, 242)
 MASK_INPUT_SHAPE = (128, 128)
 
 sched = BlockingScheduler()
@@ -31,10 +31,13 @@ def get_RGB(image):
     rgb = cv2.normalize(image[:,:,[2,1,0]], None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
     return rgb
 
+def resize(image, size=(242,242)):
+    return cv2.resize(image, size, interpolation=cv2.INTER_AREA)
+
 def class_inf(image_list):
     try:
         image_list = [get_RGB(im) for im in image_list]
-        image_list = [cv2.resize(img, CLASS_INPUT_SHAPE, interpolate=cv2.INTER_AREA) for img in image_list]
+        image_list = [resize(img) for img in image_list]
         np_image_list = np.array(image_list)/255
 
         class_model = ClassiModel.getInstance()
@@ -43,7 +46,7 @@ def class_inf(image_list):
     except Exception as e:
         return e
 
-def set_forest_view(acceptable_forests):
+def set_forest_view(acceptable_forests, start_date, end_date):
     today = datetime.combine(date.today(), datetime.min.time())
     sentinel_client = SentilhubClient()
     try:
@@ -54,7 +57,7 @@ def set_forest_view(acceptable_forests):
             forest_boundary = forest['boundary']
             updatable = forest_id in acceptable_forests
             if updatable:
-                forest_view = sentinel_client.get_forest(forest_boundary)
+                forest_view = sentinel_client.get_forest(forest_boundary, start_date, end_date)
                 preprocessed_view = np.clip(forest_view* 3.5/255, 0,1)
                 bin_image = pickle.dumps(preprocessed_view)
                 update_requests.append(UpdateOne({'_id' : forest_id}, {'$set' : {'entire_forest_view' : bin_image, 'forest_view_updated_date' : today}}))
@@ -71,136 +74,177 @@ def input_creator(image1, image2):
     return inf_img
 
 ## daily satellite feed extraction
-@sched.scheduled_job('cron', hour=14, name="get_forest_tiles")
+@sched.scheduled_job('cron', hour=21, minute=47, name="get_forest_tiles")
 def save_tiles_daily():
     dict_list = get_all_forest_tiles()
-    sentinel_client = SentilhubClient()
+    sentinel_client = SentilhubClient()    ## Init sentinelHUb invoker
     threat_list = ['agriculture', 'cultivation', 'habitation', 'road', 'water']
-    mask_model = MaskModel.getInstance()
+    mask_model = MaskModel.getInstance()    ## Init mask classification model
 
+    ## define date details 
     today = date.today()
     end_date = today - timedelta(days=1)
     start_date = today - timedelta(days=2)
 
-    documents = []
-    updatable_forests = []
-    for doc in dict_list:
+    today_dt = datetime.combine(today, datetime.min.time())
+    end_date_dt = datetime.combine(end_date, datetime.min.time())
+
+    documents = []     ## list of all the forestTiles documents 
+    updatable_forests = []  ## forest list with threat detected
+    for doc in dict_list:  ## run over all the forests in the db
         try:
+            ## extract single forest details
             forest_id = doc['forest_id']
             forest_tile_list = doc['tile_list']
             forest_status = doc['status']
             image_list = []
+
+            ## create ForestTile raw document for all the tiles in forest
             for tile in forest_tile_list:
                 tile_id = tile['tile_id']
                 coords = tile['bbox']
 
                 try:
-                    sat_image = sentinel_client.get_tile(coords, 10, start_date, end_date)
+                    ## get forest tile satellite image of yesterday.
+                    s_image = sentinel_client.get_tile(coords, 10, start_date, end_date)[0]
+                    sat_image = s_image[:,:,:5]
                     image_list.append(sat_image)
-                    preprocessed_image = sat_image[0]
-                    binary_image = pickle.dumps(preprocessed_image)
+                    ## binarize forest image to send in json request
+                    binary_image = pickle.dumps(sat_image)
                 except Exception as e:
                     return print(e)
 
+                ## compose data into document
                 forest_tile = {
                 "_id" : ObjectId(),
                 "forest_id" : forest_id,
                 "tile_id" : tile_id,
                 "image" : binary_image, 
                 "raw_image" : sat_image,
-                "image_shape" : sat_image[0].shape,
-                "save_time" : datetime.combine(today, datetime.min.time()),
+                "image_shape" : sat_image.shape,
+                "save_time" : today_dt,
                 }
                 documents.append(forest_tile)
             logger.info(f"[SAVE FOREST TILES] | forest_id : {forest_id}, num of forest tiles : {len(forest_tile_list)}")
 
             try:
+                ## get classification inference result on forest tiles
                 inferences  = class_inf(image_list)
                 for i, doc in enumerate(documents):
+                    ## add infer. to forestTile documents 
                     doc['class_infered'] = inferences[i]
 
                 logger.info(f"[INFERENCE FOREST TILES] | forest_id : {forest_id}, num of forest tiles : {len(inferences)}")
             except Exception as e:
-                logger.error(e)
+                return print(e)
+                # logger.error(e)
 
             
             try:
+                ## Init bulk-request list and data passing dictionary
                 update_requests_1 = []
                 masking_tiles = {'tile_id' : [], 'tile_image' : []}
                 view_collectable = False
+
                 if forest_status != "new":
+                    ## get last forest tile view in the system
                     forest_tile_today_pred = get_latest_forest_tiles(forest_id, end_date)
                     for doc in documents:
+                        ## find new threat type in the today forest tile
                         cur_threat = set(doc['class_infered'])
                         latest_threat = set(forest_tile_today_pred[tile['tile_id']]['res'])
                         diff_threat = list(latest_threat - cur_threat)
                         valid_threat = [x for x in diff_threat if x in threat_list]
                         if valid_threat != []:
+                            ## if there are new threat set forest into entire view updatable list
                             view_collectable = True
+                            ## add new threat to forestTile doc.
                             doc['infered_threat_present'] = valid_threat
+                            ## update data-passing dict.
                             masking_tiles['tile_id'].append(doc['tile_id'])
                             masking_tiles['tile_image'].append(doc['raw_image'])
-                            query = UpdateOne({'_id': doc['forest_id'], 'forest_tiles.tile_id':doc['tile_id']}, {'$set' : {'forest_tiles.$.infered_threat_present' : True, 'inference_updated_date' : today, 'update_view_id': doc['_id']}})
+                            ## create update query to forest on new tile 
+                            query = UpdateOne({'_id': doc['forest_id'], 'forest_tiles.tile_id':doc['tile_id']}, {'$set' : {'forest_tiles.$.infered_threat_present' : True, 'forest_tiles.$.inference_updated_date' : today_dt, 'forest_tiles.$.update_view_id': doc['_id']}})
                             update_requests_1.append(query)  
                 else:
+                    ## if forest is new then update all the forest tile reference and set into entire view updatable list
                     view_collectable = True
-                    update_requests_1.append(UpdateOne({'_id': doc['forest_id']}, {'$set' : {'forest_status' : "active"}}))
+                    update_requests_1.append(UpdateOne({'_id': doc['forest_id']}, {'$set' : {'status' : "active"}}))
                     for doc in documents:
-                        query = UpdateOne({'_id': doc['forest_id'], 'forest_tiles.tile_id':doc['tile_id']}, {'$set' : {'forest_tiles.$.infered_threat_present' : True, 'inference_updated_date' : today, 'update_view_id': doc['_id']}})
+                        query = UpdateOne({'_id': doc['forest_id'], 'forest_tiles.tile_id':doc['tile_id']}, {'$set' : {'forest_tiles.$.infered_threat_present' : True, 'forest_tiles.$.inference_updated_date' : today_dt, 'forest_tiles.$.update_view_id': doc['_id']}})
                         update_requests_1.append(query)  
 
                 if view_collectable:
                     updatable_forests.append(forest_id)
                 logger.info(f"[FOREST VIEW REFERENCE UPDATE] | forest_id : {forest_id}, num of forest tiles : {len(update_requests_1)}")
             except Exception as e:
-                logger.error(e)
+                return print(e)
+                # logger.error(e)
 
 
             try:
-                accessing_tiles = masking_tiles['tile_id']
-                acc_td_tiles = masking_tiles
-                acc_ys_tiles = get_tile_view_id(forest_id, accessing_tiles, end_date)
+                if forest_status != "new":
+                ## take updatable forest tiles from data-passing dict.
+                    accessing_tiles = masking_tiles['tile_id']
+                    ## take yesterday forest view
+                    acc_ys_tiles = get_tile_view_id(forest_id, accessing_tiles, end_date_dt)
 
-                image_td_dict = {}
-                for tile in acc_td_tiles:
-                    image_td_dict[tile['tile_id']] = pickle.loads(tile['image'])
+                    image_td_dict = {}
+                    for i, iid in enumerate(masking_tiles['tile_id']):
+                        image_td_dict[iid] = masking_tiles['tile_image'][i]
+                    logger.info(f"image_td_dict {image_td_dict}")
+                    # image_id_td_dict = {}
+                    # for tile in acc_td_tiles:
+                    #     image_id_td_dict[tile['tile_id']] = pickle.loads(tile['_id'])
 
-                image_id_td_dict = {}
-                for tile in acc_td_tiles:
-                    image_id_td_dict[tile['tile_id']] = pickle.loads(tile['_id'])
-
-                image_ys_dict = {}
-                for tile in acc_ys_tiles:
-                    image_ys_dict[tile['tile_id']] = pickle.loads(tile['image'])
-
-                inf_image_dict = {}
-                for i in image_td_dict:
-                    inf_image_dict[i] = input_creator(image_td_dict[i], image_ys_dict[i])
-
-                rgb_inf_images = [get_RGB(im)[:,:,1] for im in list(inf_image_dict.values())]      
-                inf_images = np.array(rgb_inf_images)
-
-                inferences = mask_model.inference(inf_images)
-
-                j = 0
-                for doc in documents:
-                    if doc['tile_id'] in accessing_tiles:
-                        doc['infered_mask'] = inferences[j,...]
-                        j+=1
-                logger.info(f"[INFERENCE FOREST MASK] | forest_id : {forest_id}, num of forest tiles : {len(inferences)}")
+                    ## load images from json
+                    image_ys_dict = {}
+                    for tile in acc_ys_tiles:
+                        image_ys_dict[tile['tile_id']] = pickle.loads(tile['image'])
+                    logger.info(f"image_ys_dict : {image_ys_dict}")
+                    inf_image_dict = {}
+                    for i in image_td_dict:
+                        inf_image_dict[i] = input_creator(image_td_dict[i][...,1], image_ys_dict[i][...,1])
+                    logger.info(f"image_ys_dict : {image_ys_dict}")
+                    # rgb_inf_images = [get_RGB(im)[:,:,1] for im in list(inf_image_dict.values())]      
+                    inf_images = np.array(inf_image_dict.values())
+                    logger.info(f"get single channel inputs : {inf_images.shape}")
+                    ## get segmentation model inference results
+                    inferences = mask_model.inference(inf_images)
+                    logger.info(f"get inference : {inferences.shape}")
+                    j = 0
+                    for doc in documents:
+                        if doc['tile_id'] in accessing_tiles:
+                            ## update forestTile doc. by mask
+                            doc['infered_mask'] = inferences[j,...]
+                            j+=1
+                            
+                    logger.info(f"[INFERENCE FOREST MASK] | forest_id : {forest_id}, num of forest tiles : {len(inferences)}")
+                else:
+                    logger.info(f"[INFERENCE FOREST MASK] | forest_id : {forest_id}, is new.")
             except Exception as e:
-                logger.error(e)
+                return print(e)
+                # logger.error(e)
+
+            try:
+                for doc in documents:
+                    doc.pop('raw_image', None)
+            except Exception as e:
+                return print(e)
 
         except Exception as e:
             return print(e)
 
         
     try:
+        ## insert all the today forestTile doc. to db
         result = save_forestTile(documents)
         logger.info(f"[WRITE FOREST TILES] | forest_id : {forest_id}")
+        ## update forests on todays results
         forests_bulkWrite(update_requests_1)
         logger.info(f"[UPDATE FOREST DETAIL] | forest_id : {forest_id}")
-        set_forest_view(updatable_forests)
+        ## set new forest view in forestPage Collection
+        set_forest_view(updatable_forests, end_date_dt, today_dt)
         logger.info(f"[SET FOREST VIEW] | forest_id : {forest_id}")
     except Exception as e:
         return print(e)
